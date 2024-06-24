08656f292c73cc4b5cc56767eb7988bf26a7aa35
Antônio Henrique Farias <h.farias@ter.grupomarista.org.br>
2024-06-24 10:01:03 -0300

    Refactoring

--- envio.php
+++ envio.php
@@ -1,3 +1,5 @@
+<?php require './processa_envio.php'?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -8,10 +10,11 @@
     <title>Send Mail</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
-        @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900&display=swap');
 
         * {
             font-family: "Roboto", sans-serif;
+            color: black;
         }
 
         body {
@@ -20,27 +23,17 @@
         }
 
         .container-fluid {
-            background-color: rgb(83, 158, 230, 0.1);
+            background-color: rgb(0, 0, 0, 0.0);
             backdrop-filter: blur(15px);
         }
 
-        .bg-mail {
+        .bg-infos {
             background-color: rgb(250, 250, 250, 0.7);
             border-radius: 6px;
         }
 
-        .form-control {
-            background-color: rgb(250, 250, 250, 0.1);
-            border: transparent;
-        }
-
-        .form-control:focus {
-            border: 1px solid #539EE6;
-        }
-
         .btn {
             background-color: rgb(83, 158, 230, 0.9);
-            border: none;
             color: #f3f3f3;
             transition: 0.3s;
         }
@@ -56,7 +49,7 @@
                 padding: 0;
             }
 
-            .bg-mail {
+            .bg-infos {
                 height: 100%;
                 width: 100%;
                 border-radius: 0;
@@ -66,36 +59,36 @@
 </head>
 
 <body>
-    <main class="container-fluid text-light vh-100 d-flex justify-content-center align-items-center">
-        <div class="bg-mail p-5 text-dark shadow-lg">
-
+    <div class="container-fluid text-light vh-100 d-flex justify-content-center align-items-center">
+        <div class="bg-infos p-5 shadow-lg">
             <div class="text-center">
                 <img class="d-block mx-auto mb-2" src="logo.png" alt="logo" width="72" height="72">
                 <h2>Send Mail</h2>
                 <p class="lead">Seu app de envio de e-mails particular!</p>
             </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <?php
+                    if ($mensagem->status['codigo_status'] == 1) { ?>
+                        <div class="container text-center">
+                            <h1 class="display-5 mt-5 text-success">Sucesso!</h1>
+                            <p><?php echo $mensagem->status['descricao_status']; ?></p>
+                            <a href="index.php" class="btn btn-lg mt-4">Voltar</a>
+                        </div>
+                    <?php } ?>
 
-            <form action="processa_envio.php" method="post">
-                <div class="form-group">
-                    <label for="para">Para</label>
-                    <input name="para" type="text" class="form-control shadow-sm" id="para" placeholder="usuario@dominio.com.br">
+                    <?php
+                    if ($mensagem->status['codigo_status'] == 2) { ?>
+                        <div class="container text-center">
+                            <h1 class="display-5 mt-5 text-danger">Erro ao enviar!</h1>
+                            <p><?php echo $mensagem->status['descricao_status']; ?></p>
+                            <a href="index.php" class="btn btn-lg mt-4">Voltar</a>
+                        </div>
+                    <?php } ?>
                 </div>
-
-                <div class="form-group mt-3">
-                    <label for="assunto">Assunto</label>
-                    <input name="assunto" type="text" class="form-control shadow-sm" id="assunto" placeholder="Assundo do e-mail">
-                </div>
-
-                <div class="form-group mt-3">
-                    <label for="mensagem">Mensagem</label>
-                    <textarea name="mensagem" class="form-control shadow-sm" id="mensagem"></textarea>
-                </div>
-
-                <button type="submit" class="btn btn-primary shadow mt-3 w-100">Enviar Mensagem</button>
-            </form>
-
+            </div>
         </div>
-    </main>
+    </div>
 </body>
 
 </html>
--- processa_envio.php
+++ processa_envio.php
@@ -1,5 +1,3 @@
-<?php
-
 // Importação biblioteca PHPMailer
 require './libs/PHPMailer/Exception.php';
 require './libs/PHPMailer/OAuthTokenProvider.php';
@@ -48,22 +46,21 @@ if (!$mensagem->mensagemValida()) {
     header('Location: index.php');
 }
 
-// Lógica para enviar e-mail
 $mail = new PHPMailer(true);
 
 try {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'a.henriquefarias96@gmail.com
-';
+    $mail->Username = 'a.henriquefarias96@gmail.com';
     $mail->Password = 'bhwa vtwu gtdo zyxb';
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
-    $mail->setFrom('a.henriquefarias96@gmail.com', 'Remetente');
+    $mail->setFrom('a.henriquefarias96@gmail.com');
     $mail->addAddress($mensagem->__get('para'));
 
+    $mail->CharSet = 'UTF-8';
     $mail->isHTML(true);
     $mail->Subject = $mensagem->__get('assunto');
     $mail->Body = $mensagem->__get('mensagem');
@@ -77,99 +74,3 @@ try {
 }
 
 ?>
-
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="logo.png" type="image/x-icon">
-    <title>Send Mail</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900&display=swap');
-
-        * {
-            font-family: "Roboto", sans-serif;
-            color: black;
-        }
-
-        body {
-            background-image: url('./background.png');
-            background-size: cover;
-        }
-
-        .container-fluid {
-            background-color: rgb(0, 0, 0, 0.0);
-            backdrop-filter: blur(15px);
-        }
-
-        .bg-infos {
-            background-color: rgb(250, 250, 250, 0.7);
-
-            border-radius: 6px;
-            transition: 0.5s;
-        }
-
-        .btn {
-            background-color: rgb(83, 158, 230, 0.9);
-            color: #f3f3f3;
-            transition: 0.3s;
-        }
-
-        .btn:hover {
-            background-color: rgb(83, 158, 230);
-            color: white;
-            transform: scale(1.03);
-
-        }
-
-        @media (max-width: 575px) {
-            .container-fluid {
-                padding: 0;
-            }
-
-            .bg-infos {
-                height: 100%;
-                width: 100%;
-                border-radius: 0;
-            }
-        }
-    </style>
-</head>
-
-<body>
-    <div class="container-fluid text-light vh-100 d-flex justify-content-center align-items-center">
-        <div class="bg-infos p-5 shadow-lg">
-            <div class="text-center">
-                <img class="d-block mx-auto mb-2" src="logo.png" alt="logo" width="72" height="72">
-                <h2>Send Mail</h2>
-                <p class="lead">Seu app de envio de e-mails particular!</p>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <?php
-                    if ($mensagem->status['codigo_status'] == 1) { ?>
-                        <div class="container text-center">
-                            <h1 class="display-5 mt-5 text-success">Sucesso!</h1>
-                            <p><?php echo $mensagem->status['descricao_status']; ?></p>
-                            <a href="index.php" class="btn btn-lg mt-4">Voltar</a>
-                        </div>
-                    <?php } ?>
-
-                    <?php
-                    if ($mensagem->status['codigo_status'] == 2) { ?>
-                        <div class="container text-center">
-                            <h1 class="display-5 mt-5 text-danger">Erro ao enviar!</h1>
-                            <p><?php echo $mensagem->status['descricao_status']; ?></p>
-                            <a href="index.php" class="btn btn-lg mt-4">Voltar</a>
-                        </div>
-                    <?php } ?>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
-
-</html>
